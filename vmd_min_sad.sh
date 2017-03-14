78d83c8188d1f8f7fc317597258757728d451fd5
Bharat <bharatsharma61@hotmail.com>
2017-03-14 15:55:48 -0400

    Update vmd_min_sad.sh

--- vmd_min_sad.sh
+++ vmd_min_sad.sh
@@ -5,8 +5,8 @@
 #usage           :sh script.sh
 # There is no support and no warranty. Use this script at your own risk.
 #==============================================================================
-
-
+rm energy.dat  output.xyz
+output=${1?output filename is missing!}
 
 wc -l ../min1000 > aa
 nline=$(awk '{ printf $1}' ./aa)
@@ -25,6 +25,10 @@ coorLineNumber=3
 
 for z in $list;do
 
+# energy
+energy=$(awk 'NR==2' ../$z)
+printf  "$z $energy \n" >>energy.dat
+
 printf  "$natoms\n" >>temp.xyz
 printf "MOLECULAR TITLE $z\n" >>temp.xyz
 
@@ -34,7 +38,8 @@ awk 'FNR=='$coorLineNumber+$i' {print $0}' ../${z} >>temp.xyz
 done
 done
 
-sed 's/\([^ ]*\) [6]*[6][6]* /\1 C /; s/\([^ ]*\) [8]*[8][8]* /\1 O /; s/\([^ ]*\) [1]*[1][1]* /\1 H /; s/\([^ ]*\) [7]*[7][7]* /\1 N /' temp.xyz > output_ggg.xyz
+sed 's/\([^ ]*\) [6]*[6][6]* /\1 C /; s/\([^ ]*\) [8]*[8][8]* /\1 O /; s/\([^ ]*\) [1]*[1][1]* /\1 H /; s/\([^ ]*\) [7]*[7][7]* /\1 N /' temp.xyz > $output
+
 
 #clearing files
 rm temp.xyz aa accepted
